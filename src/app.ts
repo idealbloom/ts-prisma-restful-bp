bb5119bafd4d68bc01ad4520b0fe652448635faf
hjkangIB <hjkang@idealbloom.io>
2022-04-01 17:23:45 +0900

    chore: eslint, prettier 설정(airbnb style)

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, {Request, Response, NextFunction } from 'express';
-import testRouter, {funcA, funcB} from './routes/test';
+import express, { Request, Response } from 'express';
+import testRouter from './routes/test.ts';
+
 const app: express.Application = express();
 
 app.use('/test', testRouter);
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('hello world!!!');
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world!!!');
 });
 
 // app.get('/resJsonTest', (req: express.Request, res: express.Response) => {
@@ -16,7 +17,6 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 //     ]);
 // });
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`ts-express Server listening on port: ${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`ts-express Server listening on port: ${process.env.PORT}`);
 });
--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,14 +1,13 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
 const testRouter: express.Application = express();
 
-export const funcA = async (req:Request, res:Response) => {
-    res.json("this is funcA");
-}
-export const funcB = async (req:Request, res:Response) => {
-    res.json("this is funcB");
-}
-
+export const funcA = async (req: Request, res: Response) => {
+  res.json('this is funcA');
+};
+export const funcB = async (req: Request, res: Response) => {
+  res.json('this is funcB');
+};
 
 testRouter.get('/funcA', funcA);
 testRouter.get('/funcB', funcB);
