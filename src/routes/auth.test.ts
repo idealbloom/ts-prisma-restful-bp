ff62fdf29579c65bb83638ecd66aad73701f6138
hjkangIB <hjkang@idealbloom.io>
2022-04-28 13:24:27 +0900

    test: auth.test.ts E2E 테스트중 테스트 Data를 테스트 스크립트 실행전 자체 주입후 테스트 하도록 수정함(데이터 독립적으로)

--- src/routes/auth.test.ts
+++ src/routes/auth.test.ts
@@ -2,17 +2,56 @@ import request from 'supertest';
 import app from '@src/app';
 import prisma from '@src/prisma';
 import { User } from '@prisma/client';
-import { ibDefs, IBResFormat } from '@src/utils';
+import { ibDefs, IBResFormat, genBcryptHash } from '@src/utils';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+
+let testData: {
+  email: string;
+  password: string;
+  name: string;
+};
+let testUser: User;
+
+beforeAll(async () => {
+  testData = {
+    email: 'IBJestTest99@gmail.com',
+    password: 'qwer1234',
+    name: 'idealbloom',
+  };
+
+  testUser = await prisma.user.upsert({
+    where: {
+      email: testData.email,
+    },
+    update: {
+      email: testData.email,
+      password: genBcryptHash(testData.password),
+      name: testData.name,
+    },
+    create: {
+      email: testData.email,
+      password: genBcryptHash(testData.password),
+      name: testData.name,
+    },
+  });
+});
+afterAll(async () => {
+  await prisma.user.delete({
+    where: {
+      email: testUser.email,
+    },
+  });
+});
 
 describe('Auth Express Router E2E Test', () => {
   describe('POST /signIn', () => {
     it('Case: Correct', async () => {
       const response = await request(app).post('/auth/signIn').send({
-        email: 'hapsody@gmail.com',
-        password: 'qwer1234!',
+        email: testData.email,
+        password: testData.password,
       });
+
       const result: IBResFormat = response.body as IBResFormat;
       expect(result.IBcode).toEqual({ ...ibDefs.SUCCESS }.IBcode);
     });
@@ -28,7 +67,7 @@ describe('Auth Express Router E2E Test', () => {
 
     it('Case: Incorrect password', async () => {
       const response = await request(app).post('/auth/signIn').send({
-        email: 'hapsody@gmail.com',
+        email: testData.email,
         password: 'errorrrrrr',
       });
       const result = response.body as IBResFormat;
